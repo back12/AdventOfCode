8ff0825994e1dff1875c2f92c113dcbf54b784fc
jw <jw1614@foxmail.com>
2025-12-02 14:08:11 +0800

    Fix day 1 input file path

--- src/y2025/day1/Part2.kt
+++ src/y2025/day1/Part2.kt
@@ -7,7 +7,7 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val timeMills = measureTimeMillis {
         var res = 0
-        File(".\\src\\y2025.day1\\input.txt").bufferedReader().use { reader ->
+        File(".\\src\\y2025\\day1\\input.txt").bufferedReader().use { reader ->
             var current = 50
             reader.forEachLine { line ->
                 var num = line.substring(1).toInt()
